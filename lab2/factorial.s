76d52db2e8b0636a9ec5af976901c9f6248f533b
shark bytes <sharkbytescs@gmail.com>
2025-06-06 21:30:31 -0400

    Create factorial.s

---
+++ lab2/factorial.s
@@ -0,0 +1,107 @@
+	.file	"factorial.cpp"
+	.text
+#APP
+	.globl _ZSt21ios_base_library_initv
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Please enter a number between 1 and 5: "
+.LC1:
+	.string	"calculating the factorial of "
+.LC2:
+	.string	" * "
+.LC3:
+	.string	" = "
+#NO_APP
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2012:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$1, -4(%rbp)
+	movl	$.LC0, %esi
+	movl	$_ZSt4cout, %edi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$_ZSt3cin, %edi
+	call	_ZNSirsERi
+	movl	$.LC1, %esi
+	movl	$_ZSt4cout, %edi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rdx
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi
+	movl	$_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, %esi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E
+	movl	$1, -8(%rbp)
+	jmp	.L2
+.L5:
+	movl	-4(%rbp), %eax
+	imull	-8(%rbp), %eax
+	movl	%eax, -4(%rbp)
+	movl	-12(%rbp), %eax
+	cmpl	%eax, -8(%rbp)
+	je	.L3
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	movl	$_ZSt4cout, %edi
+	call	_ZNSolsEi
+	movl	$.LC2, %esi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	jmp	.L4
+.L3:
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	movl	$_ZSt4cout, %edi
+	call	_ZNSolsEi
+	movl	$.LC3, %esi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi
+	movl	$_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, %esi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E
+.L4:
+	addl	$1, -8(%rbp)
+.L2:
+	movl	-12(%rbp), %eax
+	cmpl	%eax, -8(%rbp)
+	jle	.L5
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2012:
+	.size	main, .-main
+	.section	.rodata
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.ident	"GCC: (GNU) 14.2.1 20240912 (Red Hat 14.2.1-3)"
+	.section	.note.GNU-stack,"",@progbits
